80fa30c7fcea8407ab1813958e3eb47939f7326d
legionDroid <brunolegion@hotmail.com>
2019-12-09 15:28:25 -0300

    add chamada para API no eventos.component

--- ProAgil-WebApp/src/app/componentes/eventos/eventos.component.ts
+++ ProAgil-WebApp/src/app/componentes/eventos/eventos.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: "app-eventos",
-  templateUrl: "./eventos.component.html",
-  styleUrls: ["./eventos.component.css"]
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
   eventos: any;
@@ -16,7 +16,7 @@ export class EventosComponent implements OnInit {
   }
 
   getEventos() {
-    this.http.get("http://localhost:5000/api/evento").subscribe(response => {
+    this.http.get('http://localhost:5000/api/evento').subscribe(response => {
       this.eventos = response;
       console.log(response);
     }, error => {
